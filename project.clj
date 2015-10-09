a1823b0aa3f402427636d973823090bf5f0bda00
ayato_p <ayato.skycrawlers@gmail.com>
2015-10-09 23:04:32 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject kuromadoushi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject kuromadoushi "0.1.0"
+  :description "Kuromadoushi Renderer"
+  :url "https://github.com/ayato-p/kuromadoushi"
+  :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
