36db6b964523625dbd585e3c518a1b1c26066902
ayato_p <ayato.skycrawlers@gmail.com>
2015-10-09 22:40:24 +0900

    Extend render function

--- src/kuromadoushi/core.clj
+++ src/kuromadoushi/core.clj
@@ -1,6 +1,9 @@
 (ns kuromadoushi.core
-  (:require [clojure.string :as str]
-            [kuromadoushi.ansi :as ka]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [kuromadoushi.ansi :as ka])
+  (:import [java.io File]
+           [java.io BufferedReader]))
 
 (def space "\u3000")
 
@@ -27,8 +30,20 @@
          str/join)))
 
 (defprotocol Renderer
-  (render [this] "render function"))
+  (render* [this] "render function"))
 
 (extend-protocol Renderer
   String
-  (render [this] (-render this)))
+  (render* [this] (-render this))
+
+  File
+  (render* [this]
+    (render* (with-open [r (io/reader this)]
+               (str/join "\n" (line-seq r)))))
+
+  BufferedReader
+  (render* [this]
+    (render* (str/join "\n" (line-seq this)))))
+
+(defn render [x]
+  (println (render* x)))
